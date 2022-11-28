a347a5898f09b16f01bd984657e7bb52216e5a14
Adam Garza <boxeigit@gmail.com>
2022-11-28 02:29:31 -0600

    Build framework ground

---
+++ src/Square/basics.py
@@ -0,0 +1,13 @@
+class Basics:
+    """A class with basic functions used to test the functionality of Square.
+    """
+    def introduction(self, name=str):
+        """A simple function to introduce a user with a given name.
+
+        Args:
+            name (str): A name used for the introduction
+
+        Returns:
+            str: An introduction with the argument of name.
+        """
+        return f'Hello {name}! This was an introduction made from the package called Square.'
